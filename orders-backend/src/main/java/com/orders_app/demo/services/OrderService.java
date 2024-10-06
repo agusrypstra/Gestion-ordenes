558ecf2b88a6bcd9015d64e86b664beb163fd747
agusrypstra <agusrypstra@gmail.com>
2024-10-06 12:40:23 -0300

    Metodos findAll y save creados

---
+++ orders-backend/src/main/java/com/orders_app/demo/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.orders_app.demo.services;
+
+import com.orders_app.demo.DTO.OrderDTO;
+import com.orders_app.demo.mappers.OrderMapper;
+import com.orders_app.demo.models.OrderModel;
+import com.orders_app.demo.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<OrderDTO> findAllOrders() {
+        return orderRepository.findAll().stream()
+                .map(OrderMapper::toDTO)  // Usa el mapper
+                .collect(Collectors.toList());
+    }
+
+    public OrderModel saveOrder(OrderModel order) {
+        return orderRepository.save(order);
+    }
+}
