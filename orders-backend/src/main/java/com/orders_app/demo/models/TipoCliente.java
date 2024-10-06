ebfade1a6ef6ca8e629791a5ed58a4c64faf58e3
agusrypstra <agusrypstra@gmail.com>
2024-10-06 12:46:18 -0300

    Atributos y metodos creados

---
+++ orders-backend/src/main/java/com/orders_app/demo/models/TipoCliente.java
@@ -0,0 +1,29 @@
+package com.orders_app.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tipo_cliente")
+public class TipoCliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="nombre_tipo",nullable = false)
+    private String nombre_tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre_tipo() {
+        return nombre_tipo;
+    }
+
+    public void setNombre_tipo(String nombre_tipo) {
+        this.nombre_tipo = nombre_tipo;
+    }
+}
