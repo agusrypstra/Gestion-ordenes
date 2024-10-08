9e9280c9c67b470e4602b972b123827e78cbfe91
agusrypstra <agusrypstra@gmail.com>
2024-10-08 09:47:23 -0300

    Mapper de orden modificada

--- orders-backend/src/main/java/com/orders_app/demo/mappers/OrderMapper.java
+++ orders-backend/src/main/java/com/orders_app/demo/mappers/OrderMapper.java
@@ -1,9 +1,9 @@
 package com.orders_app.demo.mappers;
 
-import com.orders_app.demo.DTO.OrderDTO;
-import com.orders_app.demo.models.ClienteModel;
+import com.orders_app.demo.DTO.order.OrderDTO;
+import com.orders_app.demo.models.ClientModel;
 import com.orders_app.demo.models.OrderModel;
-import com.orders_app.demo.models.TipoOrdenModel;
+import com.orders_app.demo.models.OrderTypeModel;
 import com.orders_app.demo.models.UsuarioModel;
 
 public class OrderMapper {
@@ -22,10 +22,10 @@ public class OrderMapper {
     public static OrderModel toEntity(OrderDTO dto) {
         OrderModel order = new OrderModel();
         order.setId(dto.getId());
-        order.setCliente(new ClienteModel(dto.getClienteId()));  // Supongamos que tienes un constructor adecuado
+        order.setCliente(new ClientModel(dto.getClienteId()));  // Supongamos que tienes un constructor adecuado
         order.setDescripcion(dto.getDescripcion());
         order.setFechaCreacion(dto.getFechaCreacion());
-        order.setTipoOrden(new TipoOrdenModel(dto.getIdTipoOrden()));
+        order.setTipoOrden(new OrderTypeModel(dto.getIdTipoOrden()));
         order.setUsuarioCreador(new UsuarioModel(dto.getIdUsuarioCreador()));
         return order;
     }
