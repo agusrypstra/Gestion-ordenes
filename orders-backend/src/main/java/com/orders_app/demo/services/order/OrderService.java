8489618db9d359194a8b88a046fdc3c13a800c5e
agusrypstra <agusrypstra@gmail.com>
2024-10-12 00:13:51 -0300

    Modificados para funcionar con ordereREST,creado el update

--- orders-backend/src/main/java/com/orders_app/demo/services/order/OrderService.java
+++ orders-backend/src/main/java/com/orders_app/demo/services/order/OrderService.java
@@ -2,11 +2,15 @@ package com.orders_app.demo.services.order;
 
 import com.orders_app.demo.DTO.order.OrderDTO;
 import com.orders_app.demo.mappers.OrderMapper;
+import com.orders_app.demo.models.ClientModel;
 import com.orders_app.demo.models.OrderModel;
+import com.orders_app.demo.models.OrderTypeModel;
 import com.orders_app.demo.repositories.OrderRepository;
+import com.orders_app.demo.rest.OrderREST;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,7 +25,9 @@ public class OrderService {
                 .map(OrderMapper::toDTO)  // Usa el mapper
                 .collect(Collectors.toList());
     }
-    public OrderModel saveOrder(OrderModel order) {
+    public OrderModel saveOrder(OrderREST orderREST) {
+        OrderModel order = OrderMapper.toEntity(orderREST);
+        order.setFechaCreacion(LocalDateTime.now());
         return orderRepository.save(order);
     }
     public boolean deleteOrder(Long id) {
@@ -32,5 +38,21 @@ public class OrderService {
         } else {
             return false;
         }
+    }
+    public OrderModel updateOrder(Long id, OrderREST orderREST) {
+        // Buscar la orden por ID
+        OrderModel existingOrder = orderRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Order not found with id: " + id));
+
+        // Actualizar los atributos de la orden existente
+        existingOrder.setDescripcion(orderREST.getDescripcion());
+        existingOrder.setCliente(new ClientModel(orderREST.getClienteId()));
+        existingOrder.setTipoOrden(new OrderTypeModel(orderREST.getIdTipoOrden()));
+        existingOrder.setFechaToma(orderREST.getFechaToma());
+        existingOrder.setFechaFinalizacion(orderREST.getFechaFinalizacion());
+        // Aquí puedes actualizar otros atributos según lo que haya en el DTO
+
+        // Guardar la orden actualizada
+        return orderRepository.save(existingOrder);
     }
 }
